a154876cbc5d436341c6ee670b7c912322136f8a
qwq12738qwq <12738smp@Gmail.com>
2024-08-09 12:29:22 +0800

    Update api.py

--- Nonebot_APScheduler_setuAPI/api.py
+++ Nonebot_APScheduler_setuAPI/api.py
@@ -6,9 +6,7 @@ async def handle_image():
     response = httpx.post(config.api_url, params=image_data)
     if response.status_code == 200:
             data = response.json()
-            print('连接成功,正在分析json文件')
             image_url = data['data'][0]['urls']['original']
-            print(image_url)
             return image_url
     else:
         error = "网络错误,错误码:{response.status_code}"
